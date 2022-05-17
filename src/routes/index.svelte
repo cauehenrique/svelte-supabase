a464f161b108a6cefd5fa428d1ccd8d71695d743
cauehenrique <caue313@gmail.com>
2022-05-17 16:53:09 -0300

    update: books

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
   import type { Load } from "@sveltejs/kit"
 
   export const load: Load = async ({ fetch }) => {
-    const response = await fetch("/api/books")
+    const response = await fetch("/books")
 
     if (response.ok) {
       const books = await response.json()
