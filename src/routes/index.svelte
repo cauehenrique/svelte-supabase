e318c576b2e8c09cfd61c72e2ee90429ffcacbc9
cauehenrique <caue313@gmail.com>
2022-05-17 16:26:49 -0300

    update: index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,7 @@
 <h1>Hello World</h1>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime possimus
+  aspernatur nostrum ipsam quasi numquam quaerat doloribus dicta aperiam
+  consequatur, optio voluptate omnis tempora earum modi. Maxime quo architecto
+  sit!
+</p>
